e4d4645867955f3efad496263706bf3be29138f0
Gagik Grigoryan <doggedg@gmail.com>
2024-06-24 18:28:45 +0400

    Added tests and CLS rule

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,21 +1,55 @@
-import { describe, test, it, expect } from "vitest";
+import { describe, test, vi, expect } from "vitest";
 
-describe('Get list of products', () => {
+const API_URL = 'https://api.jsonbin.io/v3/b/6630fd9be41b4d34e4ecd1f9';
+global.fetch = vi.fn()
+
+function createFetchResponse(data) {
+  return { json: () => new Promise((resolve) => resolve(data)) }
+}
+
+describe('Get mock products', () => {
+  test('makes a GET request to fetch the result', async () => {
+    const products = [
+      {
+        "id": 3,
+        "name": "The Physic",
+        "ibu": 45,
+        "abv": 4.2,
+        "description": "The Physic beer...",
+        "image_url": "http://mfreitas.sytes.net:8080/images/45.png"
+      },
+      {
+        "id": 4,
+        "name": "Anarchism Alchemist",
+        "ibu": 42,
+        "abv": 2.4,
+        "description": "Anarchism Alchemist",
+        "image_url": "http://mfreitas.sytes.net:8080/images/46.png"
+      },
+    ];
+
+    fetch.mockResolvedValue(createFetchResponse(products))
+
+    const response = await fetch(API_URL)
+    const data = await response.json();
+
+    expect(fetch).toHaveBeenCalledWith(
+      API_URL
+    )
+
+    expect(data).toStrictEqual(products)
+  })})
+
+describe('Test real: Get list of products', () => {
   let response;
-  let body;
   let products;
 
   beforeAll(async () => {
     response = await fetch(
       'https://api.jsonbin.io/v3/b/6630fd9be41b4d34e4ecd1f9',
     );
-    body = await response.json();
-    products = body.record;
+    products = await response.json();
   }, 2000);
-
-  test('Should have response status 200', () => {
-    expect(response.status).toBe(200);
-  });
 
   test('Should have array in the body', () => {
     expectTypeOf(products).toBeArray();
